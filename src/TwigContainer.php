34ff5346c36ef511e5b9615afd5f13933e21fd51
Andrew Dunn <andrew.dunn@silverstripe.com>
2025-02-26 16:16:47 +1300

    Update TwigContainer.php

--- src/TwigContainer.php
+++ src/TwigContainer.php
@@ -5,7 +5,7 @@ namespace Azt3k\SS\Twig;
 use Pimple\Container;
 use Twig\Environment;
 use Twig\Extension\DebugExtension;
-use Twig\Loader\FilesystemLoader
+use Twig\Loader\FilesystemLoader;
 
 class TwigContainer extends Container
 {
